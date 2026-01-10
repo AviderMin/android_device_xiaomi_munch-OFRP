43dd2e2ba1160de12b4308f8b0f1da96090b37a9
AviderMin <95974013+AviderMin@users.noreply.github.com>
2026-01-10 19:39:59 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -43,6 +43,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_SETTINGS_ROOT_DIRECTORY="/metadata/OFRP"
 	export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/sdcard"
 	export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
+	
+    # Move the Magisk installer/uninstaller ZIP to the ramdisk
+	export FOX_MOVE_MAGISK_INSTALLER_TO_RAMDISK=1
 
     # Dynamic Partition Assist Options
     export FOX_USE_DMSETUP=1
