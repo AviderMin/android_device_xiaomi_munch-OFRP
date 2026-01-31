cca00e58bfa591356cfbe561502dbb0eaac34d49
AviderMin <95974013+AviderMin@users.noreply.github.com>
2026-01-31 17:46:33 +0800

    Enable ZSTD binary usage in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -60,6 +60,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_DELETE_AROMAFM=1
 	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_ZSTD_BINARY=1
 
 	# instruct magiskboot v24+ to always patch the vbmeta header when patching the recovery/boot image; do *not* remove!
         export FOX_PATCH_VBMETA_FLAG="1"
